16271bcc133b99bd9e829a1e2864908945f331de
cegemba <chyegembaj@gmail.com>
2014-12-17 12:28:48 -0600

    Initial commit

---
+++ AllJavaProjects/src/com/chidi/project1/Calculation.java
@@ -0,0 +1,15 @@
+package com.chidi.project1;
+
+public class Calculation {
+	int x = 5;
+	int y = 10;
+	
+	public int getSum(){
+		
+	
+	System.out.println("The sum of  x + y is " + (x + y));
+	
+	return (x + y);
+	}
+
+}
---
+++ AllJavaProjects/src/com/chidi/project1/TestCalculation.java
@@ -0,0 +1,12 @@
+package com.chidi.project1;
+
+public class TestCalculation {
+
+	public static void main(String[] args) {
+		Calculation cal  = new Calculation();
+		cal.getSum();
+		// TODO Auto-generated method stub
+
+	}
+
+}
